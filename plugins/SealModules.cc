01baba11646f76e00f924cda2e3c0daf5f7cd99e
Edmund <edmund.a.berry@cern.ch>
2014-10-18 12:55:41 +0200

    Added trigger objects

--- plugins/SealModules.cc
+++ plugins/SealModules.cc
@@ -6,6 +6,9 @@
 #include "HCALPFG/HcalTupleMaker/interface/HcalTupleMaker_FEDs.h"
 #include "HCALPFG/HcalTupleMaker/interface/HcalTupleMaker_HcalDigis.h"
 #include "HCALPFG/HcalTupleMaker/interface/HcalTupleMaker_HcalRecHits.h"
+#include "HCALPFG/HcalTupleMaker/interface/HcalTupleMaker_Trigger.h"
+#include "HCALPFG/HcalTupleMaker/interface/HcalTupleMaker_TriggerObjects.h"
+#include "HCALPFG/HcalTupleMaker/interface/HcalCosmicDigisProducer.h"
 
 DEFINE_FWK_MODULE(HcalTupleMaker_Tree);
 DEFINE_FWK_MODULE(HcalTupleMaker_Event);
@@ -16,3 +19,6 @@ DEFINE_FWK_MODULE(HcalTupleMaker_HFDigis);
 DEFINE_FWK_MODULE(HcalTupleMaker_HBHERecHits);
 DEFINE_FWK_MODULE(HcalTupleMaker_HORecHits);
 DEFINE_FWK_MODULE(HcalTupleMaker_HFRecHits);
+DEFINE_FWK_MODULE(HcalTupleMaker_Trigger);
+DEFINE_FWK_MODULE(HcalTupleMaker_TriggerObjects);
+DEFINE_FWK_MODULE(HcalCosmicDigisProducer);

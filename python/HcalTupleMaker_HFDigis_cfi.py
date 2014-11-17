47b0f4f74d974790f6935f48f16df3a05bf25321
Edmund <edmund.a.berry@cern.ch>
2014-11-17 16:15:21 +0100

    Added uTCA digis module

--- python/HcalTupleMaker_HFDigis_cfi.py
+++ python/HcalTupleMaker_HFDigis_cfi.py
@@ -9,6 +9,15 @@ hcalTupleHFDigis = cms.EDProducer("HcalTupleMaker_HFDigis",
   DoEnergyReco = cms.untracked.bool ( True ) 
 )
 
+hcalTupleHFUTCADigis = cms.EDProducer("HcalTupleMaker_HFDigis",
+  source  = cms.untracked.InputTag("utcaDigis"),
+  recHits = cms.untracked.InputTag("hfreco"),
+  Prefix  = cms.untracked.string ( "HFUTCADigi"),
+  Suffix  = cms.untracked.string ( ""),
+  DoChargeReco = cms.untracked.bool ( True ) ,
+  DoEnergyReco = cms.untracked.bool ( True ) 
+)
+
 hcalTupleHFL1JetsDigis = cms.EDProducer("HcalTupleMaker_HFDigis",
   source  = cms.untracked.InputTag("hcalL1JetDigis"),
   recHits = cms.untracked.InputTag("hfreco"),

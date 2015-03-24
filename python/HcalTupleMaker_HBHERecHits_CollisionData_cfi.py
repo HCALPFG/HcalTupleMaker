8f77515e61e5719c3524854d88f7e85f546ce36c
dertexaner <halilsaka@gmail.com>
2015-03-24 01:46:56 +0100

    Changed hbheprereco to hbhereco for HBHEIsolatedNoiseReflagger

--- python/HcalTupleMaker_HBHERecHits_CollisionData_cfi.py
+++ python/HcalTupleMaker_HBHERecHits_CollisionData_cfi.py
@@ -5,13 +5,19 @@ import FWCore.ParameterSet.Config as cms
 # http://cmslxr.fnal.gov/lxr/source/RecoLocalCalo/HcalRecProducers/python/HcalHitReconstructor_hbhe_cfi.py #
 # For example, by default, this has: samplesToAdd = cms.int32(2) PLUS other settings                       #
 ############################################################################################################
-from Configuration.StandardSequences.Reconstruction_Data_cff import hbheprereco
+# Removing this: from Configuration.StandardSequences.Reconstruction_Data_cff import hbheprereco
 #
-hbherecoMethod2 = hbheprereco.clone()
+# Isolation filter (in data reco sequence) is set via: 
+# http://cmslxr.fnal.gov/lxr/source/RecoLocalCalo/HcalRecProducers/python/HBHEIsolatedNoiseReflagger_cfi
+# This produces hbhereco from hbheprereco (also applying HBHEIsolatedNoiseReflagger).
+# So we should use hbhereco instead.
+#
+from Configuration.StandardSequences.Reconstruction_Data_cff import hbhereco
+#
+hbherecoMethod2 = hbhereco.clone()
 hbherecoMethod2.puCorrMethod = cms.int32(2)
-hbherecoMethod0 = hbheprereco.clone()
+hbherecoMethod0 = hbhereco.clone()
 hbherecoMethod0.puCorrMethod = cms.int32(0)
-
 
 hcalTupleHBHERecHits = cms.EDProducer("HcalTupleMaker_HBHERecHits",
                                       source = cms.untracked.InputTag("hbherecoMethod2"),

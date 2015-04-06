c4e659812ab1b10d589615ae441f82db98f96f46
Edmund <edmund.a.berry@cern.ch>
2015-04-06 09:50:58 +0200

    Added BCN and ORN

--- plugins/HcalTupleMaker_Event.cc
+++ plugins/HcalTupleMaker_Event.cc
@@ -5,6 +5,8 @@ HcalTupleMaker_Event::HcalTupleMaker_Event(const edm::ParameterSet& iConfig) {
   produces <unsigned int> ( "run"    );
   produces <unsigned int> ( "event"  );
   produces <unsigned int> ( "ls"     );
+  produces <unsigned int> ( "bcn"    );
+  produces <unsigned int> ( "orn"    );
 }
 
 void HcalTupleMaker_Event::
@@ -13,9 +15,13 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   std::auto_ptr<unsigned int >  run   ( new unsigned int(iEvent.id().run()        ) );
   std::auto_ptr<unsigned int >  event ( new unsigned int(iEvent.id().event()      ) );
   std::auto_ptr<unsigned int >  ls    ( new unsigned int(iEvent.luminosityBlock() ) );
+  std::auto_ptr<unsigned int >  bcn   ( new unsigned int(iEvent.bunchCrossing()   ) );
+  std::auto_ptr<unsigned int >  orn   ( new unsigned int(iEvent.orbitNumber()     ) );
 
   iEvent.put( run,   "run"   );
   iEvent.put( event, "event" );
   iEvent.put( ls   , "ls"    );
+  iEvent.put( bcn  , "bcn"   );
+  iEvent.put( orn  , "orn"   );
 
 }

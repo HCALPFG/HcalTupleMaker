9b300835406d290861ed0abb99a857602aa98169
dertexaner <halilsaka@gmail.com>
2015-06-28 19:01:16 +0200

    Updated HLT prescales access

--- interface/HcalTupleMaker_Trigger.h
+++ interface/HcalTupleMaker_Trigger.h
@@ -5,6 +5,7 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
+#include "HLTrigger/HLTcore/interface/HLTPrescaleProvider.h"
 
 class HcalTupleMaker_Trigger : public edm::EDProducer {
  public:
@@ -24,6 +25,8 @@ class HcalTupleMaker_Trigger : public edm::EDProducer {
   std::string                 sourceName;
   DataSource                  sourceType;
   std::vector<std::string>    dataSource;
+
+  HLTPrescaleProvider hltPrescaleProvider;
 
 };
 

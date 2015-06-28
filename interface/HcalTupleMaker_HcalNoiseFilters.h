b5baa4a3a85ea503024d45d799204c410191c299
dertexaner <halilsaka@gmail.com>
2015-06-28 23:03:58 +0200

    Added flag for RECO data compatibility

--- interface/HcalTupleMaker_HcalNoiseFilters.h
+++ interface/HcalTupleMaker_HcalNoiseFilters.h
@@ -14,6 +14,7 @@ class HcalTupleMaker_HcalNoiseFilters : public edm::EDProducer {
   edm::InputTag   noiseSummaryInputTag;
   std::string     noiseResultInputTag;
   std::string     recoInputTag;
+  bool            isRAW;
   std::string     prefix,suffix;
 };
 

51532c2e80a794e35bee672c7365b97b8a445ba9
lucien1011 <lucien1011@gmail.com>
2015-09-24 17:22:38 +0200

    added BX number

--- plugins/HcalTupleMaker_Event.cc
+++ plugins/HcalTupleMaker_Event.cc
@@ -1,10 +1,12 @@
 #include "HCALPFG/HcalTupleMaker/interface/HcalTupleMaker_Event.h"
 #include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Common/interface/EventBase.h"
 
 HcalTupleMaker_Event::HcalTupleMaker_Event(const edm::ParameterSet& iConfig) {
   produces <unsigned int> ( "run"    );
   produces <unsigned int> ( "event"  );
   produces <unsigned int> ( "ls"     );
+  produces <unsigned int> ( "bx"     );
 }
 
 void HcalTupleMaker_Event::
@@ -14,8 +16,12 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   std::auto_ptr<unsigned int >  event ( new unsigned int(iEvent.id().event()      ) );
   std::auto_ptr<unsigned int >  ls    ( new unsigned int(iEvent.luminosityBlock() ) );
 
+  edm::EventBase const & eventbase = iEvent;
+  std::auto_ptr<unsigned int >  bx    ( new unsigned int(eventbase.bunchCrossing() ) );
+  
   iEvent.put( run,   "run"   );
   iEvent.put( event, "event" );
   iEvent.put( ls   , "ls"    );
+  iEvent.put( bx   , "bx"    );
 
 }

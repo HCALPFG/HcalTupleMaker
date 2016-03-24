4fd114445f1173140b64a36155dd347567ba0290
lucien1011 <lucien1011@gmail.com>
2016-03-24 13:51:09 +0100

    customized RawToDigi sequence

--- test/pfg_CommissioningCosmic_RAW_cfg.py
+++ test/pfg_CommissioningCosmic_RAW_cfg.py
@@ -10,9 +10,9 @@ import FWCore.ParameterSet.VarParsing as VarParsing
 # Declare the process and input variables
 #------------------------------------------------------------------------------------
 process = cms.Process('PFG')
-inputFiles = "/store/data/Commissioning2016/MinimumBias/RAW/v1/000/265/655/00000/8A5C40D3-E1DC-E511-82EE-02163E012A67.root"
+inputFiles = "/store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/266/667/00000/F23DB80D-62E8-E511-8D81-02163E014403.root"
 skipEvents = 0
-processEvents = 1000
+processEvents = 1500
 outputFile = "results.root"
 
 #------------------------------------------------------------------------------------
@@ -36,7 +36,7 @@ process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.MagneticField_0T_cff')
 process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
 process.load('Configuration.StandardSequences.ReconstructionCosmics_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
@@ -48,17 +48,56 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_Tree_cfi")
 process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_Event_cfi")
 process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HBHEDigis_cfi")
+process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HODigis_cfi")
+process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HFDigis_cfi")
+process.load("HCALPFG.HcalTupleMaker.HcalCosmicDigisProducer_cfi")
 process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HBHERecHits_cfi")
 process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HFRecHits_cfi")
 #process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_Trigger_cfi")
 process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_MuonTrack_cfi")
+process.load("HLTrigger.HLTfilters.triggerResultsFilter_cfi")
 
+from Configuration.StandardSequences.RawToDigi_Data_cff import *
+process.CustomizedRawToDigi = cms.Sequence(
+		gtDigis*
+		siPixelDigis*
+		siStripDigis*
+		ecalDigis*
+		ecalPreshowerDigis*
+		hcalDigis*
+		muonDTDigis*
+		muonCSCDigis*
+		muonRPCDigis*
+		castorDigis*
+		scalersRawToDigi*
+		tcdsDigis
+)
 
 #------------------------------------------------------------------------------------
 # Specify Global Tag
 #------------------------------------------------------------------------------------
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 process.GlobalTag.globaltag = '80X_dataRun2_Prompt_v4'
+
+#------------------------------------------------------------------------------------
+# Configure modules
+#------------------------------------------------------------------------------------
+process.hcalTupleTree.outputCommands = cms.untracked.vstring(
+        'drop *',
+        'keep *_hcalTupleEvent_*_*',
+        'keep *_hcalTupleHBHECosmicsDigis_*_*',
+        #'keep *_hcalTupleHBHERecHits_*_*',
+        'keep *_hcalTupleTrigger_*_*',
+        'keep *_hcalTupleCosmicMuons_*_*',
+    )
+
+process.my_hlt = cms.EDFilter("HLTHighLevel",
+     TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
+     HLTPaths = cms.vstring("HLT_L1SingleMuOpen_*"), # provide list of HLT paths (or patterns) you want
+     eventSetupPathsKey = cms.string(''), # not empty => use read paths from AlCaRecoTriggerBitsRcd via this key
+     andOr = cms.bool(True),             # how to deal with multiple triggers: True (OR) accept if ANY is true, False (AND) accept if ALL are true
+     throw = cms.bool(False)    # throw exception on unknown path names
+)
 
 #------------------------------------------------------------------------------------
 # HcalTupleMaker sequence definition
@@ -71,8 +110,9 @@ process.tuple_step = cms.Sequence(
     #    # Make HCAL tuples: digi info
     #raw
     process.hcalTupleHBHEDigis*
-    #    process.hcalTupleHODigis*
-    #    process.hcalTupleHFDigis*
+    #process.hcalTupleHODigis*
+    #process.hcalTupleHFDigis*
+    process.hcalCosmicDigis*
     #    process.hcalTupleTriggerPrimitives*
     #    # Make HCAL tuples: digi info
     process.hcalTupleHBHECosmicsDigis*
@@ -97,7 +137,7 @@ process.tuple_step = cms.Sequence(
     
     #    process.hcalTupleTriggerObjects*
     #    # Make HCAL tuples: cosmic muon info
-    #    process.hcalTupleCosmicMuons*
+    # process.hcalTupleCosmicMuons*
     #    # Package everything into a tree
     #
     process.hcalTupleTree
@@ -108,8 +148,9 @@ process.tuple_step = cms.Sequence(
 # Path and EndPath definitions
 #-----------------------------------------------------------------------------------
 process.preparation = cms.Path(
-    #process.my_hlt *
-    process.RawToDigi * #needed for RAW files
+    process.my_hlt *
+    #process.RawToDigi * #needed for RAW files
+    process.CustomizedRawToDigi * 
     #process.L1Reco *
     #process.reconstruction * #needed for RAW files
     #process.caloglobalreco *

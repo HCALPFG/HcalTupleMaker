1e4e551047d4ae8838c19d7e5430ac84575aa2ee
David <david.renhwa.yu@gmail.com>
2016-04-25 07:23:08 -0500

    Add Jae's local cfg

---
+++ test/pfg_Local_RAW_cfg.py
@@ -0,0 +1,127 @@
+#------------------------------------------------------------------------------------
+# Imports
+#------------------------------------------------------------------------------------
+import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.Eras import eras
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+
+#------------------------------------------------------------------------------------
+# Declare the process and input variables
+#------------------------------------------------------------------------------------
+process = cms.Process('PFG')
+inputFiles = "file:/afs/cern.ch/user/j/jaehyeok/work/scratch/USC_269836.root"
+skipEvents = 0
+processEvents = 2000 
+outputFile = "results.root"
+
+#------------------------------------------------------------------------------------
+# Get and parse the command line arguments
+#------------------------------------------------------------------------------------
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(processEvents) )
+process.source = cms.Source("HcalTBSource",
+    fileNames  = cms.untracked.vstring(inputFiles),
+    skipEvents = cms.untracked.uint32(skipEvents),
+)
+
+process.TFileService = cms.Service("TFileService",
+     fileName = cms.string(outputFile)
+)
+
+#------------------------------------------------------------------------------------
+# import of standard configurations
+#------------------------------------------------------------------------------------
+process.load('Configuration.Geometry.GeometryIdeal_cff')
+process.load('FWCore.MessageLogger.MessageLogger_cfi')
+process.load("EventFilter.HcalRawToDigi.HcalRawToDigi_cfi")
+process.load("RecoLocalCalo.Configuration.hcalLocalReco_cff")
+process.hbhereco = process.hbheprereco.clone()
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
+
+#------------------------------------------------------------------------------------
+# Set up our analyzer
+#------------------------------------------------------------------------------------
+#process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_cfi") # Dont want to use this, load modules individually
+process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_Tree_cfi")
+process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HcalUnpackerReport_cfi")
+process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_Event_cfi")
+process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HBHEDigis_cfi")
+process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HODigis_cfi")
+process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HFDigis_cfi")
+
+#------------------------------------------------------------------------------------
+# Since this is a local run, make sure we're looking for the FEDs in the right place
+#------------------------------------------------------------------------------------
+process.hcalDigis.InputLabel = cms.InputTag("source")
+
+#------------------------------------------------------------------------------------
+# FED numbers
+#------------------------------------------------------------------------------------
+#process.hcalDigis.FEDs = cms.untracked.vint32(1100, 1102, 1104, 1106, 1108, 1110, 1112, 1114, 1116)
+
+
+#------------------------------------------------------------------------------------
+# Specify Global Tag
+#------------------------------------------------------------------------------------
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.GlobalTag.globaltag = '80X_dataRun2_Prompt_v4'
+
+#------------------------------------------------------------------------------------
+# HcalTupleMaker sequence definition
+#------------------------------------------------------------------------------------
+process.tuple_step = cms.Sequence(
+    # Make HCAL tuples: Event, run, ls number
+    process.hcalTupleEvent*
+    # Make HCAL tuples: FED info
+    #process.hcalTupleFEDs*
+    # Make HCAL tuples: unpacker info
+    #process.hcalTupleUnpackReport*
+    # Make HCAL tuples: digi info
+    process.hcalTupleHBHEDigis*
+    process.hcalTupleHODigis*
+    process.hcalTupleHFDigis*
+    #process.hcalCosmicDigis*
+    #    process.hcalTupleTriggerPrimitives*
+    #    # Make HCAL tuples: digi info
+    #process.hcalTupleHBHECosmicsDigis*
+    #    process.hcalTupleHOCosmicsDigis*
+    #    # Make HCAL tuples: digi info
+    #    process.hcalTupleHBHEL1JetsDigis*
+    #    process.hcalTupleHFL1JetsDigis*
+    #    process.hcalTupleL1JetTriggerPrimitives*
+    #    # Make HCAL tuples: reco info
+    #process.hcalTupleHBHERecHits*
+    #process.hcalTupleHFRecHits*
+    #process.hcalTupleHcalNoiseFilters*
+    #process.hcalTupleMuonTrack*
+    #
+    #process.hcalTupleHBHERecHitsMethod0*
+    #process.hcalTupleHcalNoiseFiltersMethod0*
+    #process.hcalTupleCaloJetMetMethod0*
+    #    process.hcalTupleHORecHits*
+    #    process.hcalTupleHFRecHits*
+    #    # Trigger info
+    #process.hcalTupleTrigger*
+    
+    #    process.hcalTupleTriggerObjects*
+    #    # Make HCAL tuples: cosmic muon info
+    # process.hcalTupleCosmicMuons*
+    #    # Package everything into a tree
+    #
+    process.hcalTupleTree
+)
+
+
+#-----------------------------------------------------------------------------------
+# Path and EndPath definitions
+#-----------------------------------------------------------------------------------
+process.preparation = cms.Path(
+    # Unpack digis from RAW
+    process.hcalDigis*
+    # Do energy reconstruction
+#    process.hbhereco*
+#    process.horeco*
+#    process.hfreco*
+    # Make the ntuples
+    process.tuple_step
+)

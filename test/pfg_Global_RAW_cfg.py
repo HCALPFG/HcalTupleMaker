00d4628fd802180b17d0e9fc302dc8be3fac7560
lucien1011 <lucien1011@gmail.com>
2017-05-10 15:57:30 +0200

    port to 90X

--- test/pfg_Global_RAW_cfg.py
+++ test/pfg_Global_RAW_cfg.py
@@ -8,8 +8,8 @@ import FWCore.ParameterSet.VarParsing as VarParsing
 #------------------------------------------------------------------------------------
 # Declare the process and input variables
 #------------------------------------------------------------------------------------
-process = cms.Process('PFG')
-inputFiles = "/store/data/Run2016B/JetHT/RAW/v2/000/273/502/00000/0029695E-AC1B-E611-8E15-02163E011934.root"
+process = cms.Process('PFG',eras.Run2_2017)
+inputFiles = "root://eoscms//store/caf/user/ccecal/TPG/splash2017_run293591_all.root"
 skipEvents = 0
 processEvents = 10
 outputFile = "HcalTupleMaker.root"
@@ -31,11 +31,9 @@ process.TFileService = cms.Service("TFileService",
 # import of standard configurations
 #------------------------------------------------------------------------------------
 process.load('Configuration.StandardSequences.Services_cff')
-process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_0T_cff')
 process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
 process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
@@ -46,61 +44,17 @@ process.load("CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi")
 #------------------------------------------------------------------------------------
 # Set up our analyzer
 #------------------------------------------------------------------------------------
-#process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_cfi") # Dont want to use this, load modules individually
-process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_Tree_cfi")
-process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_Event_cfi")
-process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HBHEDigis_cfi")
-process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HODigis_cfi")
-process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HFDigis_cfi")
+process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_cfi") # Dont want to use this, load modules individually
 process.hcalTupleHFDigis.DoEnergyReco = False
-process.hcalTupleHFDigis.FilterChannels = True
+process.hcalTupleHFDigis.FilterChannels = False
 process.hcalTupleHFDigis.ChannelFilterList = cms.untracked.VPSet(
-    # -------------------------------------------------
-    # HF channels for QIE10 study
-    # 26 = (24+2) QIE10 channels
-    # 22 = (24-2) dual readout QIE8 channels
-    # 2 dead QIE8 channels replaced with QIE10 channels
-    # -------------------------------------------------
-    # depth 1
+	# Notice the only channel listed here will be saved, if the FilterChannels flag is set to true
     cms.PSet(iEta = cms.int32(29), iPhi = cms.int32(39), depth = cms.int32(1)),
-    cms.PSet(iEta = cms.int32(30), iPhi = cms.int32(39), depth = cms.int32(1)),
-    cms.PSet(iEta = cms.int32(31), iPhi = cms.int32(39), depth = cms.int32(1)),
-    cms.PSet(iEta = cms.int32(32), iPhi = cms.int32(39), depth = cms.int32(1)),
-    cms.PSet(iEta = cms.int32(33), iPhi = cms.int32(39), depth = cms.int32(1)),
-    cms.PSet(iEta = cms.int32(34), iPhi = cms.int32(39), depth = cms.int32(1)),
-    cms.PSet(iEta = cms.int32(35), iPhi = cms.int32(39), depth = cms.int32(1)),
-    cms.PSet(iEta = cms.int32(36), iPhi = cms.int32(39), depth = cms.int32(1)),
-    cms.PSet(iEta = cms.int32(37), iPhi = cms.int32(39), depth = cms.int32(1)),
-    cms.PSet(iEta = cms.int32(38), iPhi = cms.int32(39), depth = cms.int32(1)),
-    cms.PSet(iEta = cms.int32(39), iPhi = cms.int32(39), depth = cms.int32(1)),
-    cms.PSet(iEta = cms.int32(41), iPhi = cms.int32(39), depth = cms.int32(1)),
-    # depth 2
-    cms.PSet(iEta = cms.int32(29), iPhi = cms.int32(39), depth = cms.int32(2)),
-    cms.PSet(iEta = cms.int32(30), iPhi = cms.int32(39), depth = cms.int32(2)), # dead
-    cms.PSet(iEta = cms.int32(31), iPhi = cms.int32(39), depth = cms.int32(2)),
-    cms.PSet(iEta = cms.int32(32), iPhi = cms.int32(39), depth = cms.int32(2)),
-    cms.PSet(iEta = cms.int32(33), iPhi = cms.int32(39), depth = cms.int32(2)),
-    cms.PSet(iEta = cms.int32(34), iPhi = cms.int32(39), depth = cms.int32(2)), # dead
-    cms.PSet(iEta = cms.int32(35), iPhi = cms.int32(39), depth = cms.int32(2)),
-    cms.PSet(iEta = cms.int32(36), iPhi = cms.int32(39), depth = cms.int32(2)),
-    cms.PSet(iEta = cms.int32(37), iPhi = cms.int32(39), depth = cms.int32(2)),
-    cms.PSet(iEta = cms.int32(38), iPhi = cms.int32(39), depth = cms.int32(2)),
-    cms.PSet(iEta = cms.int32(39), iPhi = cms.int32(39), depth = cms.int32(2)),
-    cms.PSet(iEta = cms.int32(41), iPhi = cms.int32(39), depth = cms.int32(2))
     )
-process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HBHERecHits_cfi")
-process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HFRecHits_cfi")
-process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_L1Jets_cfi")
-process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_HcalTriggerPrimitives_cfi")
-process.load("HCALPFG.HcalTupleMaker.HcalL1JetDigisProducer_cfi")
-#process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_Trigger_cfi")
-process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_MuonTrack_cfi")
-process.load("HLTrigger.HLTfilters.triggerResultsFilter_cfi")
-process.load("HCALPFG.HcalTupleMaker.HcalTupleMaker_QIE10Digis_cfi")
 
 from Configuration.StandardSequences.RawToDigi_Data_cff import *
 process.CustomizedRawToDigi = cms.Sequence(
-                gtDigis*
+        gtDigis*
 		#siPixelDigis*
 		#siStripDigis*
 		#ecalDigis*
@@ -115,32 +69,33 @@ process.CustomizedRawToDigi = cms.Sequence(
 )
 
 #------------------------------------------------------------------------------------
+# FED numbers 
+#------------------------------------------------------------------------------------
+#process.hcalDigis.FEDs = cms.untracked.vint32(	1100, 1102, 1104, # HBHEa 
+#                                              	1106, 1108, 1110, # HBHEb
+#                                              	1112, 1114, 1116, # HBHEc
+#                                              	1118, 1120, 1122, # HF
+#                                              	1119, 1121, 1123  # HF
+#												724, 725, 726, 727, 728, 729, 730, 731, # HO
+# 												1134, # HcalLaser
+#                                              )
+
+#------------------------------------------------------------------------------------
 # QIE10  Unpacker
 #------------------------------------------------------------------------------------
 process.qie10Digis = process.hcalDigis.clone()
-#process.qie10Digis.InputLabel = cms.InputTag("source") 
-process.qie10Digis.FEDs = cms.untracked.vint32(1132)
+#process.qie10Digis.FEDs = cms.untracked.vint32(1118,1120,1122,1119,1121,1123)
 
 #------------------------------------------------------------------------------------
 # Specify Global Tag
 #------------------------------------------------------------------------------------
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-process.GlobalTag.globaltag = '80X_dataRun2_Prompt_v8'
+process.GlobalTag.globaltag = '90X_dataRun2_Prompt_v3'
 
 #------------------------------------------------------------------------------------
-# Configure modules
-#------------------------------------------------------------------------------------
-process.my_hlt = cms.EDFilter("HLTHighLevel",
-     TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
-     HLTPaths = cms.vstring("HLT_L1SingleMuOpen_*"), # provide list of HLT paths (or patterns) you want
-     eventSetupPathsKey = cms.string(''), # not empty => use read paths from AlCaRecoTriggerBitsRcd via this key
-     andOr = cms.bool(True),             # how to deal with multiple triggers: True (OR) accept if ANY is true, False (AND) accept if ALL are true
-     throw = cms.bool(False)    # throw exception on unknown path names
-)
-
-#------------------------------------------------------------------------------------------------------------------------------------
 # Create Noise Filter
-#------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+# Could be out of date, commented out in the main sequence, need to be fixed
 process.hcalnoise.fillCaloTowers = cms.bool(False)
 process.hcalnoise.fillTracks = cms.bool(False)
 process.ApplyBaselineHBHENoiseFilter = cms.EDFilter('BooleanFlagFilter',
@@ -152,46 +107,31 @@ process.ApplyBaselineHBHENoiseFilter = cms.EDFilter('BooleanFlagFilter',
 # HcalTupleMaker sequence definition
 #------------------------------------------------------------------------------------
 process.tuple_step = cms.Sequence(
-    # Make HCAL tuples: Event, run, ls number
-    process.hcalTupleEvent*
-    # Make HCAL tuples: FED info
-    #    process.hcalTupleFEDs*
-    #    # Make HCAL tuples: digi info
-    #raw
-    #process.hcalTupleHBHEDigis*
-    #process.hcalTupleHODigis*
+    
+	## Make HCAL tuples: Event, run, ls number
+    #process.hcalTupleEvent*
+    
+	## Make HCAL tuples: FED info
+    #process.hcalTupleFEDs*
+    
+	## Make HCAL tuples: digi info
+    process.hcalTupleHBHEDigis*
+    process.hcalTupleHODigis*
     process.hcalTupleHFDigis*
     process.hcalTupleQIE10Digis*
-    #process.hcalCosmicDigis*
-    #process.hcalL1JetDigis*
-    #process.hcalTupleTriggerPrimitives*
-    #    # Make HCAL tuples: digi info
-    #process.hcalTupleHBHECosmicsDigis*
-    #    process.hcalTupleHOCosmicsDigis*
-    #    # Make HCAL tuples: digi info
-    #process.hcalTupleHBHEL1JetsDigis*
-    #    process.hcalTupleHFL1JetsDigis*
-    #    process.hcalTupleL1JetTriggerPrimitives*
-    #    # Make HCAL tuples: reco info
+     
+	## Make HCAL tuples: reco info
     #process.hcalTupleHBHERecHits*
-    #process.hcalTupleL1Jets*
     #process.hcalTupleHFRecHits*
-    #process.hcalTupleHcalNoiseFilters*
-    #process.hcalTupleMuonTrack*
-    #
-    #process.hcalTupleHBHERecHitsMethod0*
-    #process.hcalTupleHcalNoiseFiltersMethod0*
-    #process.hcalTupleCaloJetMetMethod0*
-    #    process.hcalTupleHORecHits*
-    #    process.hcalTupleHFRecHits*
-    #    # Trigger info
+    #process.hcalTupleHORecHits*
+    #process.hcalTupleHFRecHits*
+
+    ## Trigger info
     #process.hcalTupleTrigger*
-    
-    #    process.hcalTupleTriggerObjects*
-    #    # Make HCAL tuples: cosmic muon info
-    # process.hcalTupleCosmicMuons*
-    #    # Package everything into a tree
-    #
+    #process.hcalTupleTriggerPrimitives*
+    #process.hcalTupleTriggerObjects*
+
+    ## Package everything into a tree
     process.hcalTupleTree
 )
 
@@ -199,36 +139,16 @@ process.tuple_step = cms.Sequence(
 # Path and EndPath definitions
 #-----------------------------------------------------------------------------------
 process.preparation = cms.Path(
-    #process.my_hlt *
     #process.RawToDigi * #needed for RAW files
     #process.CustomizedRawToDigi *
     process.hcalDigis*
     process.qie10Digis*
     #process.L1Reco *
     #process.reconstruction * #needed for RAW files
-    #process.caloglobalreco *
     #process.hcalLocalRecoSequence *
     #
     #process.horeco *
     #process.hfreco *
-    #
-    #process.hbheprerecoMethod0 *
-    #process.hbheprerecoMethod2 *
-    #process.hbherecoMethod0 *
-    #process.hbherecoMethod2 *
-    #
-    #process.towerMakerMethod0 *
-    #process.towerMakerMethod2 *
-    #
-    #process.hcalnoiseMethod0 *
-    #process.hcalnoiseMethod2 *
-    #
-    #process.HBHENoiseFilterResultProducerMethod0 *
-    #process.HBHENoiseFilterResultProducerMethod2 *
-    #
-    #
-    #process.hcalCosmicDigis *
-    #process.hcalL1JetDigis *
     #
     #process.hcalnoise *  #needed for RAW files
     #process.HBHENoiseFilterResultProducer *
